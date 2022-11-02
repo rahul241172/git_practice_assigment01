b4e7d5186063065a82ecb8d88c6b45acdfe01cdc
rahul241172 <rahulgandhi241172@gmail.com>
2022-11-02 11:31:27 +0530

    check if it prime

---
+++ prime.js
@@ -0,0 +1,11 @@
+function prime(num){
+    if(num<=1 || num==undefined){
+        return false
+    }
+    for(let i=2;i<=num**0.5;i++){
+        if(num%i==0){
+            return false
+        }
+    }
+    return true
+}
